3a43dc0252b3bd8d90fe1d3d67ed1930a391f0eb
Von Cedriec Abuloc <www.prometheusvon@gmail.com>
2024-04-27 19:55:03 +0800

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkt6oLFWpMWANnN064eBb1WLnvUvVRddE",
+  authDomain: "item-borrowing-system.firebaseapp.com",
+  projectId: "item-borrowing-system",
+  storageBucket: "item-borrowing-system.appspot.com",
+  messagingSenderId: "425070832005",
+  appId: "1:425070832005:web:b06ffbf0b4acca93cf7cec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
