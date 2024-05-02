03833a7294a5ba0abcff0f1fe9deed99fa99f6fe
Von Cedriec Abuloc <www.prometheusvon@gmail.com>
2024-05-02 20:29:33 +0800

    committed as of 5/2 20-29

--- src/Pages/Admin/AddItem.jsx
+++ src/Pages/Admin/AddItem.jsx
@@ -1,13 +1,24 @@
-import { Box, Grid, TextField } from '@mui/material'
-import React from 'react'
+import { Box, Button, CircularProgress, Grid, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import AdminDrawer from './AdminDrawer'
 import Header from '../Header'
 import * as yup from 'yup'
 import { Form, Formik } from 'formik'
+import { doc, getFirestore, setDoc } from 'firebase/firestore'
+import { toast } from 'react-toastify'
+import { CloudUpload, UploadFile } from '@mui/icons-material'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 
 const AddItem = () => {
   
-  
+    //elements declaration
+    const db = getFirestore();
+    const storage = getStorage();
+    const [openLoad, setOpenLoad] = useState(false);
+    const [openWithNum, setOpenWithNum] = useState(false)
+    const [progress, setProgress] = useState('')
+    const [itemImage, setItemImage] = useState(null);
+
 
 
   //form-related codes
@@ -28,17 +39,98 @@ const AddItem = () => {
       genSpecs: yup.string().required("This field is required"),
       location: yup.string().required("This field is required")
     })
+
   
     //uploading data to Firestore
-    const uploadData = (values) => {
+    const uploadData = async (values) => {
+        setOpenLoad(true)
 
+        await setDoc(doc(db, "Items", values.iic), {
+          ...values, 
+          itemImage: itemImage
+
+        }).then(() => {
+          setOpenLoad(false)
+          toast.success("Data uploaded successfully!")
+        }).catch((error) => {
+          toast.info(`Error occured due to: ${error}`)
+        })
     }
-  
-  
+
+    //change handler for the image
+    const imageChanger = (event) => {
+      setOpenWithNum(true);
+      const file = event.target.files[0];
+      const storageRef = ref(storage, `item-images/${file.name}`)
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setProgress(progress);
+          },
+          (error) => {
+            toast.info(`File upload disrupted due to: ${error}`);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setItemImage(downloadURL);
+              setOpenWithNum(false);
+              toast.success("File uploaded successfully!")
+            });
+          }
+        );
+    };
+
+
   return (
-    <Box>  
+    <Box display='grid'> 
+
+     {/* Loading element */}
+      {openLoad !== false && (
+          <CircularProgress 
+          sx={{ color: 'blue',     
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 9999, }}
+          size="80px" 
+          /> 
+        )} 
+
+      {/* Loading element with number */}
+      {openWithNum !== false && (
+        <Box>
+        <CircularProgress 
+        sx={{ color: 'blue',     
+              position: 'fixed',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 9999, }}
+        size="80px" 
+          />
+          <Box
+          sx={{
+            top: '55.5%',
+            left: '52.5%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 9999,
+            position: 'fixed'
+          }}
+        >
+          <Typography variant="caption" component="div" color="white">
+                {`${Math.round(progress)}%`}
+          </Typography>
+        </Box>
+        </Box>
+      )} 
+
+      
       <Header title='Add item/material' description='Create a new item/material that is ready for students/teachers to borrow'/>
-        <Box m='20px' justifyContent='center' alignSelf='center' display='flex' marginTop='100px'>
+        <Box m='20px' justifyContent='space-around' alignSelf='center' display='flex' marginTop='100px'>
             <Formik 
                 initialValues={initialValues}
                 validationSchema={validation}
@@ -46,7 +138,8 @@ const AddItem = () => {
             >
               {({values, errors, touched, handleChange, handleBlur}) => (
                 <Form>
-                  <Box m='5px' >
+                  {/* First textfields */}
+                  <Box m='5px'>
                   <Grid container spacing={2}>
                     <Grid item>
                     <TextField
@@ -92,11 +185,100 @@ const AddItem = () => {
                     </Grid>
                   </Grid>
                   </Box>
+
+                      {/* Second textfields */}
+                  <Box m='5px' marginTop='40px'>
+                  <Grid container spacing={2}>
+                    <Grid item>
+                    <TextField
+                        fullWidth
+                        variant='filled'
+                        type='text'
+                        label='Brand Model'
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.brandModel}
+                        name='brandModel'
+                        error={!!touched.brandModel && !!errors.brandModel}
+                        helperText={touched.brandModel && errors.brandModel}
+                        sx={{
+                          width: '300px',
+                          gridColumn: 'span 2',
+                          "& .MuiInputBase-root":{
+                            color: 'white'
+                          }
+                        }}
+                        />
+                    </Grid>
+                    <Grid item>
+                    <TextField
+                        fullWidth
+                        variant='filled'
+                        type='text'
+                        label='General Specifications'
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.genSpecs}
+                        name='genSpecs'
+                        error={!!touched.genSpecs && !!errors.genSpecs}
+                        helperText={touched.genSpecs && errors.genSpecs}
+                        sx={{
+                          width: '300px',
+                          gridColumn: 'span 2',
+                          "& .MuiInputBase-root":{
+                            color: 'white'
+                          }
+                        }}
+                        />
+                    </Grid>
+                  </Grid>
+                  </Box>
+
+                        {/* Third textfield and a button */}
+                  <Box m='5px' marginTop='40px'>
+                    <Grid>
+                    <TextField
+                        fullWidth
+                        variant='filled'
+                        type='text'
+                        label='Location'
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.location}
+                        name='location'
+                        error={!!touched.location && !!errors.location}
+                        helperText={touched.location && errors.location}
+                        sx={{
+                          width: '300px',
+                          gridColumn: 'span 2',
+                          "& .MuiInputBase-root":{
+                            color: 'white'
+                          }
+                        }}
+                        />                    
+                        <Button variant='contained' component='label'
+                            startIcon={<CloudUpload/>}
+                            sx={{marginLeft: '20px', background: 'brown'}}
+                        >
+                          Upload a photo
+                          <input style={{display: 'none'}} type='file' accept='image/' onChange={imageChanger} id='imageinput'/>
+                        </Button>
+                    </Grid>
+                  </Box>
                   
+                  <Box marginTop='20px' marginLeft='5px' justifyContent='center' alignSelf='center' display='flex'>
+                    <Button startIcon={<UploadFile/>} type='submit' variant='contained' sx={{width: '100%', background: 'brown'}}
+                    >Upload data to Database
+                    </Button>
+                  </Box>
                 </Form>
               )}
 
             </Formik>
+
+            <Box>
+              Heil
+            </Box>
         </Box>
       <AdminDrawer/>
     </Box>
--- src/Pages/Admin/Admin_Dash.jsx
+++ src/Pages/Admin/Admin_Dash.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import AdminDrawer from './AdminDrawer'
+import Header from '../Header'
 
 
 const Admin_Dash = ({user}) => {
@@ -8,12 +9,11 @@ const Admin_Dash = ({user}) => {
   return (
 
     <Box m='20px'>
-      <Box>
-      You are at the admin dash
-        <AdminDrawer user={user}/>
-       
+      <Header title="Dashboard" description="This is the dashboard"/>
+      <Box display='flex' justifyContent='center'>
+       Heil
       </Box>
-
+      <AdminDrawer user={user}/>
     </Box>
   )
 }
